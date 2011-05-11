70ae68b79a5532375d3f728c1a92e9443f31a3a7
meruru117@gmail.com <meruru117@gmail.com@e67a9241-7432-0128-daa1-2b41893b16fa>
2011-05-11 17:04:27 +0000

    v0.1.1

--- trunk/Globals.java
+++ trunk/Globals.java
@@ -86,22 +86,189 @@ public class Globals {
 		public static int CHI = 300;
 		public static int SELFKAN = 200;
 		public static int RIICHI = 201;
+		public static int TENPAI = 1000;
+		public static int NOTEN = 1001;
 	}
 	
 	//ID numbers
 	public static int SCORESCREEN = 1;
 	
 	//Others
-	public static String VERSION = "v0.1.0 Alpha";
+	public static String VERSION = "v0.1.1 Alpha";
+	
+	public static class Powers{
+		public static int drawBased = 0;
+		public static int invisibility = 1;
+		public static int COUNT = 2;
+	}
 	
 	//Character IDs
 	public static class Characters{
-		public static int GENERIC = 0;
-		public static int SAKI = 1;
-		public static int KANA = 2;
-		public static int KOROMO = 3;
+		//public static int GENERIC = 0;
+		public static int SAKI = 0;
+		public static int KANA = 1;
+		public static int KOROMO = 2;
+		public static int MOMOKA = 3;
 		
 		public static int COUNT = 4;
+		
+		public static class Graphics{
+			public static int NEUTRAL = 0;
+			public static int HAPPY = 1;
+			public static int SAD = 2;
+			public static int FULL = 3;
+			
+			public static int COUNT = 4;
+		}
+		
+		public static String getName(int CharID, boolean bJapanese){
+			if(CharID == SAKI){
+				if(bJapanese)
+					return "宮永 咲 (みやなが　さき)";
+				return "Miyanaga Saki";
+			}
+			if(CharID == KANA){
+				if(bJapanese)
+					return "池田華菜 (いけだ　かな)";
+				return "Ikeda Kana";
+			}
+			if(CharID == KOROMO){
+				if(bJapanese)
+					return "天江 衣 (あまえ　ころも)";
+				return "Amae Koromo";
+			}
+			if(CharID == MOMOKA){
+				if(bJapanese)
+					return "東横桃子(とうよこ　ももか)";
+				return "Touyoko Momoka";
+			}
+			return "";
+		}
+		
+		public static String getSchool(int CharID, boolean bJapanese){
+			if(CharID == SAKI){
+				if(bJapanese)
+					return "清澄高校";
+				return "Kiyosumi High School";
+			}
+			if(CharID == KANA){
+				if(bJapanese)
+					return "風越女子高校";
+				return "Kazekoshi Girls' School";
+			}
+			if(CharID == KOROMO){
+				if(bJapanese)
+					return "龍門渕高校";
+				return "Ryuumonbuchi High School";
+			}
+			if(CharID == MOMOKA){
+				if(bJapanese)
+					return "鶴賀学園";
+				return "Tsuruga Academy ";
+			}
+			return "";
+		}
+		
+		/**
+		 * Return must be 6 lines!
+		 *Switching to a bullet point format because it's easier
+		 * Text size is based off of the first line, so pad it to the max amount of space needed
+		 * @param CharID
+		 * @param bJapanese
+		 * @return
+		 */
+		public static String[] getBio(int CharID, boolean bJapanese){
+			if(CharID == SAKI){
+				if(bJapanese)
+					return new String[] {"ちょっと内気な高校１年生の女の子。",
+										"勝ちでも負けでもない±０（プラスマイナス・ゼロ）の成績で終わる",
+										"麻雀を打ち続けていたが、「和（のどか）」との出会いで麻雀に対する",
+										"意欲が変化し始める。普段のドジで泣き虫な姿とは裏腹に、麻雀では",
+										"ここぞという場面で驚異的な打ち筋を魅せる。",
+										""};
+				return new String[] {"- First year student and anchor of her team                         \n",
+									 "- Notorious for her ability to score zero points every round",
+									 "- Great at playing in person, but can not play online",
+									 "- Her estranged sister is a top-ranked mahjong player",
+									 "- Very close with Nodoka",
+									 "- Power activates when in tenpai (if possible)"};
+			}
+			if(CharID == KANA){
+				if(bJapanese)
+					return new String[] {"風越女子高校の２年生で、校内ランキング２位の実力をもつ。福路を",
+										"心から敬愛しており、そのせいか福路にとってラストチャンスとなる今",
+										"回の県大会予選に対して熱い闘志をみなぎらせる。思い込みが強い",
+										"反面、まわりが見えなくなって、周囲から浮いてる存在となってしまう",
+										"事がたまにある。",
+										""};
+				return new String[] {"- Second year student and 2nd best player on her team                     \n",
+									 "- Despire being 2nd best player on the team, she is the team's anchor",
+									 "- Very close with Captain Mihoko",
+									 "- Plucky, known for come backs when down big",
+									 "- Nya",
+									 ""};
+			}
+			if(CharID == KOROMO){
+				if(bJapanese)
+					return new String[] {"出身、経歴などが謎につつまれた女の子。プロ雀士の藤田が「牌に",
+										"愛された子」と称する３人の女子高生のうちの一人。一見すると子供",
+										"と思われるような幼い風貌とは裏腹に、麻雀の実力では何人たりと",
+										"も寄せ付けることが出来ない程の強さを持っている。",
+										"",
+										""};
+				return new String[] {"- Second year student and the ace of the Ryūmonbuchi team                    \n",
+									 "- Looks like a little girl but, dislikes people who treat her like a child",
+									 "- Her play is enhanced under a full moon",
+									 "- Despite her acting tough, she feels lonely and is looking for friends",
+									 "- Her birthday is September 6",
+									 "- Power activates when in tenpai and the wall count < 24 (if possible)"};
+			}
+			if(CharID == MOMOKA){
+				if(bJapanese)
+					return new String[] {"鶴賀学園・麻雀部でごく希に存在が確認される高校１年生。影の薄",
+										"さ金メダル級の体質を持っており、本人も回りとのコミュニケーション",
+										"を避けていたが、ふとしたキッカケで麻雀部に入ることに。麻雀の腕が",
+										"確かなのはもちろん、その体質を利用した独自の打ち回しで周囲を",
+										"翻弄する。",
+										""};
+				return new String[] {"- First year student and #2 player on her team                              \n",
+									 "- Has a very weak presence causing her to be ignored by everyone",
+									 "- Very close with Yumi (who was the first person to notice her)",
+									 "- Not the most skilled player, realizes on her invisibility to win",
+									 "- Power activates after 2 straight rounds of no pon/chi/ron calls",
+									 ""};
+			}
+			return new String[] {"",
+								"",
+								"",
+								"",
+								"",
+								""};
+		}
+		
+		public static String getPower(int CharID, boolean bJapanese){
+			if(CharID == SAKI){
+				if(bJapanese)
+					return "嶺上開花";
+				return "Rinshan Kaihou";
+			}
+			if(CharID == KANA){
+				if(bJapanese)
+					return "無い";
+				return "None";
+			}
+			if(CharID == KOROMO){
+				if(bJapanese)
+					return "海底撈月";
+				return "Haitei Raoyue";
+			}
+			if(CharID == MOMOKA){
+				if(bJapanese)
+					return "見えない";
+				return "Invisibility";
+			}
+			return "";
+		}
 	}
 	
 	public static class Graphics{
@@ -109,13 +276,19 @@ public class Globals {
 		public static int DEFAULT_WIDTH = 480;
 		
 		//Misc constants
-		public static int MISC_BUBBLE_LEFT = 0;
-		public static int MISC_BUBBLE_RIGHT = 1;
-		public static int MISC_LOGO = 2;
-		public static int MISC_START_BTN = 3;
-		public static int MISC_OK_BTN = 4;
-		public static int MISC_CENTER = 5;
-		public static int MISC_COUNT = 6;
+		public static class MISC{
+			public static int BUBBLE_LEFT = 0;
+			public static int BUBBLE_RIGHT = 1;
+			public static int LOGO = 2;
+			public static int START_BTN = 3;
+			public static int OK_BTN = 4;
+			public static int CENTER = 5;
+			public static int LEFT_ARROW = 6;
+			public static int RIGHT_ARROW = 7;
+			public static int OK_SQUARE_BTN = 8;
+			
+			public static int COUNT = 9;
+		}
 	}
 	
 	//Scoring Table
@@ -398,6 +571,48 @@ public class Globals {
 				return "リ一チ";
 			return "Riichi";
 		}
+		if(cmd == CMD.TENPAI){
+			if(japanese)
+				return "てんぱい";
+			return "Tenpai";
+		}
+		if(cmd == CMD.NOTEN){
+			if(japanese)
+				return "のてん";
+			return "Noten";
+		}
+		return "";
+	}
+	
+	public static String LimitHandToString(int han, boolean bJapanese){
+		if(han < 5)
+			return "";
+		if(han == 5){
+			if(bJapanese)
+				return "満貫";
+			return "Mangan";
+		}
+		else if(han <= 7){
+			if(bJapanese)
+				return "跳満";
+			return "Haneman";
+		}
+		else if(han <= 9){
+			if(bJapanese)
+				return "倍満";
+			return "Baiman";
+		}
+		else if(han <= 12){
+			if(bJapanese)
+				return "三倍満";
+			return "Sanbaiman";
+		}
+		else if(han >= 13){
+			if(bJapanese)
+				return "役満";
+			return "Yakuman";
+		}
 		return "";
 	}
 }
+
